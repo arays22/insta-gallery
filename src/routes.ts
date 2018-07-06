1ef98bde1424df11f38f8e2796db12c07d55a11f
areiz <reiza6@gmail.com>
2018-07-05 19:20:24 -0500

    login component routed

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,5 @@
 import { Routes } from '@angular/router';
 import { GalleryComponent } from './app/gallery/gallery.component';
 import { ImageDetailComponent } from './app/image-detail/image-detail.component';
+import { LoginComponent } from './app/login/login.component';
 
